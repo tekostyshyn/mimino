d1ceb2fe13f4a8f694d24bb6c11737fc9c87f0e4
tekostyshyn <tekostyshyn@gmail.com>
2023-03-13 20:15:58 +0200

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ flatpickr('.calendar', {
     firstDayOfWeek: 1,
     weekdays: {
       shorthand: ['Нд', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-      longhand: ['Неділя', 'Понеділок', 'Вівторок', 'ССереда', 'Четвер', 'Пятниця', 'Субота'],         
+      longhand: ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'Пятниця', 'Субота'],         
     }, 
     months: {
       shorthand: ['Січ', 'Лют', 'Бер', 'Квіт', 'Трав', 'Черв', 'Лип', 'Серп', 'Вер', 'Жов', 'Лист', 'Груд'],
@@ -30,4 +30,23 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   $('#client-phone').inputmask('+38 (999) 999-99-99');
+});
+
+// modals
+
+const openBtn = document.getElementsByClassName('open');
+const closeBtn = document.getElementsByClassName('close');
+Array.from(closeBtn, closeButton => {
+  closeButton.addEventListener('click', el => {
+    let btnId = el.target.getAttribute('data-id');
+    document.getElementById(btnId).classList.add('is-hidden');
+    document.body.classList.remove('modal-open');
+  });
+});
+Array.from(openBtn, openButton => {
+  openButton.addEventListener('click', e => {
+    let modalId = e.target.getAttribute('data-id');
+    document.getElementById(modalId).classList.remove('is-hidden');
+    document.body.classList.add('modal-open');
+  });
 });
